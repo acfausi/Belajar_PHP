9dbec2ba73c6af0c87b4ffa3ea76c48e8857bed7
acfausi <achfausi.student@unibamadura.ac.id>
2023-03-26 00:31:16 +0700

    pembaruan

--- tugas2.php
+++ tugas2.php
@@ -7,53 +7,112 @@
     <title>Document</title>
 </head>
 <body>
-    <form action="POST">
-    <p>Form Penggajian</p>
-        <label> Nama </label>
-        <input type="text" name="nama" placeholder="Masukan nama"><br>
-        <label>Jabatan</label>
-        <select name="jabatan">
-            <option>----pilih jabatan ---</option>
-            <option value="manajer">Manajer</option>
-            <option value="asisten">Asisten</option>
-            <option value="kabag">Kabag</option>
-            <option value="staff">Staff</option>
-        </select><br>
-        <label>Anak </label>
-        <input type="text" name="anak" placeholder="masukan jumlah anak">
-        <button name="proses" type="submit">Simpan</button>
+    <h1>Data Pegawai</h1>
+    <form method = 'POST'>
+        <table >
+            <tr>
+                <td>Nama</td>
+                <td><input type='text' name='nama' style="width:100%"></td>
+            </tr>
+            <tr>
+                <td>Pekerjaan</td>
+                <td>
+                    <select name='jabatan'>
+                    <option>---Pilih Pekerjaan---</option>
+                    <option value='Manajer'>Manager</option>
+                    <option value='Asisten'>Asisten</option>
+                    <option value='Kepala_Bagian'>Kepala Bagian</option>
+                    <option value='Staff'>Staff</option>
+                    </select>
+                </td>
+            </tr>
+            <tr>
+                <td>Status</td>
+                <td>
+                    <select name='status'>
+                        <option>---Pilih Status---</option>
+                        <option value='Menikah'>Menikah</option>
+                        <option value='Belum_menikah'>Belum Menikah</option>
+                    </select>
+                </td>
+            </tr>
+            <tr>
+                <td>Anak</td>
+                <td>
+                    <input type="text" name='anak'>
+                </td>
+            </tr>
+            <tr>
+                <td>Agama</td>
+                <td>
+                    <select name='agama' style="width:100%">
+                        <option>---Pilih Agama---</option>
+                        <option value='Islam'>Islam</option>
+                        <option value='Non_Islam'>Non Islam</option>
+                    </select>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                <button name='simpan' type='submit'>Simpan</button>
+                </td>
+            </tr>
+        </table>
     </form>
+
     <?php
+    
        $nama = $_POST['nama'];
-       $pekerjaan = $_POST['pekerjaan'];
+       $jabatan = $_POST['jabatan'];
        $status = $_POST['status'];
        $anak = $_POST['anak'];
        $agama = $_POST['agama'];
 
     //    menentukan gaji
     switch ($jabatan){
-        case 'Manager' : $gaji = 20000000; break;
-        case 'Asiiten' : $gaji = 15000000; break;
+        case 'Manajer' : $gaji = 20000000; break;
+        case 'Asisten' : $gaji = 15000000; break;
         case 'Kepala_Bagian' : $gaji = 10000000; break;
         case 'Staff' : $gaji = 4000000; break;
         default: $gaji = "";
     }
 
     // tunjangan jabatan
-    $t_jabatan = 0.2 * $$gaji;
+    $t_jabatan = 0.2 * $gaji;
 
+
+    // tunjangan keluarga
     if( $status=="menikah" && $anak == 2 ){
-        $t_keluarga = 0.05 * $g_pokok;
+        $t_keluarga = 0.05 * $gaji;
     }else if( $status=="menikah" && $anak >=3 && $anak <=5){
-        $t_keluarga = 0.1 * $g_pokok;
+        $t_keluarga = 0.1 * $gaji;
     }else{
         $t_keluarga = 0;
     }
 
+    // gaji kotor
+    $kotor = $gaji + $t_jabatan + $t_keluarga;
+
+
     // zakat
-    $zakat = ($agama == "Islam" && $g_kotor >= 6000000) ? 0.025 * $g_kotor: 0;
+    $zakat = ($agama == "Islam" && $kotor >= 6000000) ? 0.025 * $kotor: 0;
 
+    // take home pay
+    $home = $kotor - $zakat;
 
+    if(isset($button)){}
     ?>
+            <br>Nama Pegawai : <?= $nama ?>
+            <br>Pekerjaan : <?= $jabatan ?>
+            <br>Gaji Pokok : <?= $gaji ?>
+            <br>Tunjangan Jabatan : <?= $t_jabatan ?>
+            <br>Status : <?= $status ?>
+            <br>Anak : <?= $anak ?>
+            <br>Tunjangan Anak : <?= $t_keluarga ?>
+            <br>Gaji Kotor : <?= $kotor ?>
+            <br>Agama : <?= $agama ?>
+            <br>Zakat : <?= $zakat ?>
+            <br>Take Home Pay : <?= $home ?>
+
 </body>
 </html>
