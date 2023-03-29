f11d2c623a93e144601c90e0eb1437d497f61bdf
acfausi <achfausi.student@unibamadura.ac.id>
2023-03-29 22:39:22 +0700

    pembaruan

--- tugas4.php
+++ tugas4.php
@@ -2,89 +2,85 @@
 $ar_prodi = ["SI"=>"Sistem Informasi", "TI"=>"Teknik Informatika","ILKOM"=>"Ilmu Komputer","TE"=>"Teknik Elektro"];
 
 $ar_skill = ["HTML"=>10,"CSS"=>10, "Javascript"=>20, "RWD Bootstrap"=>20, "PHP"=>30, "MySQL"=>30,"Laravel"=>40];
-$domisili = ["Jakarta","Bandung","Bekasi","Malang","Surabaya", "lainnya"];
+// $domisili = ["Jakarta","Bandung","Bekasi","Malang","Surabaya", "lainnya"];
 ?>
 <fieldset style="background-color:aquamarine;">
-   <h3><center> Form Registrasi Kelompok Belajar </center></h3>
-<table>
-    <thead>
-        <tr>
-            <th>Form Registrasi</th>
-        </tr>
-    </thead>
-    <tbody>
-        <form method="POST">
+    <h3>
+        <center> Form Registrasi Kelompok Belajar </center>
+    </h3>
+    <table>
+        <thead>
             <tr>
-                <td>NIM </td>
-                <td>:</td>
-                <td> 
-                    <input type="text" name="nim" >
-                </td>
+                <th>Form Registrasi</th>
             </tr>
-            <tr>
-                <td>Nama </td>
-                <td>:</td>
-                <td> 
-                    <input type="text" name="nama" >
-                </td>
-            </tr>
-            <tr>
-                <td>Jenis Kelamin</td>
-                <td>:</td>
-                <td> 
-                    <input type="radio" name="jk" value="Laki-laki" >Laki-Laki &nbsp;
-                    <input type="radio" name="jk" value="Laki-laki" >Perempuan 
-                </td>
-            </tr>
-            <tr>
-                <td>Program Studi</td>
-                <td>:</td>
-                <td> 
-                    <select name="prodi">
-                        <?php 
+        </thead>
+        <tbody>
+            <form method="POST">
+                <tr>
+                    <td>NIM </td>
+                    <td>:</td>
+                    <td>
+                        <input type="number" name="nim">
+                    </td>
+                </tr>
+                <tr>
+                    <td>Nama </td>
+                    <td>:</td>
+                    <td>
+                        <input type="text" name="nama">
+                    </td>
+                </tr>
+                <tr>
+                    <td>Jenis Kelamin</td>
+                    <td>:</td>
+                    <td>
+                        <input type="radio" name="jk" value="Laki-laki">Laki-Laki &nbsp;
+                        <input type="radio" name="jk" value="Laki-laki">Perempuan
+                    </td>
+                </tr>
+                <tr>
+                    <td>Program Studi</td>
+                    <td>:</td>
+                    <td>
+                        <select name="prodi">
+                            <?php 
 
                         foreach($ar_prodi as $prodi => $v){
                             ?>
                             <option value="<?= $prodi ?>"><?= $v ?></option>
-                       <?php } ?>
+                            <?php } ?>
                         </select>
-                </td>
-            </tr>
-            <tr>
-                <td>Skill Programming  </td>
-                <td>:</td>
-                <td> 
-                    <?php 
+                    </td>
+                </tr>
+                <tr>
+                    <td>Skill Programming </td>
+                    <td>:</td>
+                    <td>
+                        <?php 
                     foreach ($ar_skill as $skill => $s){
                         ?>
-                    <input type="checkbox" name="skill[]" value="<?= $skill ?>" ><?= $skill ?>
+                        <input type="checkbox" name="skill[]" value="<?= $skill ?>"><?= $skill ?>
 
-                    <?php } ?>
-                </td>
-            </tr>
-            <tr>
-                <td>Domisili </td>
-                <td>:</td>
-                <td> 
-                    <select name="domisili">
-                        <?php 
-
-                        foreach($domisili as $d){
-                            ?>
-                            <option value="<?= $d ?>"><?= $d ?></option>
-                       <?php } ?>
-                        </select>
-                </td>
-            </tr>
-            <tfoot>
-                <tr>
-                    <th colspan="2">
-                        <button name="proses">Submit</button>
-                    </th>
+                        <?php } ?>
+                    </td>
                 </tr>
-            </tfoot>
+                <tr>
+                    <td>Email </td>
+                    <td>:</td>
+                    <td>
+                        <input type="email" name="email">
+                    </td>
+                </tr>
+                
+        <tfoot>
+            <tr>
+                <th colspan="2">
+                    <button name="proses">Submit</button>
+                </th>
+            </tr>
+        </tfoot>
     </table>
-            
+
 </fieldset>
 
 <?php 
@@ -95,7 +91,7 @@ if(isset($_POST['proses'])){
     $jk = $_POST['jk'];
     $prodi = $_POST['prodi'];
     $skill = $_POST['skill'];
-    $domisili = $_POST['domisili'];
+    $email = $_POST['email'];
 
     $nilai = 0;
     foreach($skill as $jml) {
@@ -124,7 +120,7 @@ if(isset($_POST['proses'])){
 <fieldset>
     <table>
         <tbody>
-            <form >
+            <form>
                 <tr>
                     <td>NIM</td>
                     <td>:</td>
@@ -164,13 +160,12 @@ if(isset($_POST['proses'])){
                     <td><?= $k_skill ?></td>
                 </tr>
                 <tr>
-                    <td>Domisili</td>
+                    <td>Email</td>
                     <td>:</td>
-                    <td><?= $domisili ?></td>
+                    <td><?= $email ?></td>
                 </tr>
             </form>
         </tbody>
     </table>
-    </fieldset>
+</fieldset>
 <?php } ?>
-
