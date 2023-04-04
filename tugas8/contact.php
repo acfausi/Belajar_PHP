f002545570214e6fb5bb23679bde266a1fe153dc
acfausi <achfausi.student@unibamadura.ac.id>
2023-04-05 00:09:47 +0700

    origin master

---
+++ tugas8/contact.php
@@ -0,0 +1,18 @@
+<main id="main" class="main">
+    <p class="text-center p-5">
+      This page is only available in the pro version!
+    </p>
+  </main>
+            <footer class="py-4 bg-light mt-auto">
+                <div class="container-fluid px-4">
+                    <div class="d-flex align-items-center justify-content-between small">
+                        <div class="text-muted">Copyright &copy; Your Website 2023</div>
+                        <div>
+                            <a href="#">Privacy Policy</a>
+                            &middot;
+                            <a href="#">Terms &amp; Conditions</a>
+                        </div>
+                    </div>
+                </div>
+            </footer>
+        </div>
